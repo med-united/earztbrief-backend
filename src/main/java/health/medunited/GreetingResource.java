ca018d2a03b8cb6e4b2f8321a9b257880b561451
ruszkipista <oi5672@gmail.com>
2022-04-13 08:52:03 +0100

    add sendemail POST

--- src/main/java/health/medunited/GreetingResource.java
+++ src/main/java/health/medunited/GreetingResource.java
@@ -1,16 +1,81 @@
 package health.medunited;
 
-import javax.ws.rs.GET;
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/sendemail")
 public class GreetingResource {
+    private static Logger log = Logger.getLogger(GreetingResource.class.getName());
 
-    @GET
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public void sendERezeptToKIMAddress(String fromKimAddress, 
+                                        String toKimAddress, 
+                                        String noteToPharmacy, 
+                                        String smtpHostServer, 
+                                        String smtpUser, 
+                                        String smtpPassword, 
+                                        String eRezeptToken) {
+        try {
+            Properties props = new Properties();
+
+            props.put("mail.smtp.host", smtpHostServer);
+            props.put("mail.smtp.auth", true);
+
+            Session session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(smtpUser, smtpPassword);
+                }
+            });
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("X-KIM-Dienstkennung", "eRezept;Zuweisung;V1.0");
+
+            msg.setFrom(new InternetAddress(fromKimAddress));
+
+            msg.setReplyTo(InternetAddress.parse(fromKimAddress, false));
+
+            msg.setSubject("E-Rezept direkte Zuweisung", "UTF-8");
+
+
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(noteToPharmacy, "utf-8");
+
+            MimeBodyPart erezeptTokenPart = new MimeBodyPart();
+            erezeptTokenPart.setText(eRezeptToken, "utf8");
+            
+            Multipart multiPart = new MimeMultipart();
+            multiPart.addBodyPart(textPart); // <-- first
+            multiPart.addBodyPart(erezeptTokenPart); // <-- second
+            msg.setContent(multiPart);
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toKimAddress, false));
+            log.info("Message is ready");
+            Transport.send(msg);  
+
+            log.info("E-Mail sent successfully to: "+toKimAddress);
+        } catch (Exception e) {
+        log.log(Level.WARNING, "Error during sending E-Prescription", e);
+        }
     }
 }
