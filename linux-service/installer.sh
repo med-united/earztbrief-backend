c3ae499525aec06fd41cf6921c87a3f88f61e224
simoneS96 <simone.stifano@incentergy.de>
2022-04-15 14:23:39 +0200

    test workflow

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -3,7 +3,7 @@ cp -r ../target/quarkus-app /opt/med-united
 mkdir -p /opt/med-united/application/quarkus-app/
 mkdir -p /etc/med-united/config
 cp ../src/main/resources/application.properties /etc/med-united/config
-#ln -s /etc/med-united/config /opt/med-united/config
+ln -s /etc/med-united/config /opt/med-united/config
 #!mv /opt/med-united/server.keystore /opt/med-united/application/quarkus-app/server.keystore
 cp med-united.service /etc/systemd/system/
 sudo systemctl daemon-reload
