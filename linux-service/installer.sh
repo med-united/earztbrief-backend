255d629c7e41b6ea53673207d59bb55d8c6aa84c
simoneS96 <simone.stifano@incentergy.de>
2022-04-19 12:41:37 +0200

    change installer.sh

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cp -r ../target/quarkus-app /opt/med-united
-mkdir -p /opt/med-united/application/quarkus-app/
-mkdir -p /etc/med-united/config
-cp ../src/main/resources/application.properties /etc/med-united/config
-ln -s /etc/med-united/config /opt/med-united/config
-#!mv /opt/med-united/server.keystore /opt/med-united/application/quarkus-app/server.keystore
-cp med-united.service /etc/systemd/system/
+sudo cp -r ../target/quarkus-app /opt/med-united
+sudo mkdir -p /opt/med-united/application/quarkus-app/
+sudo mkdir -p /etc/med-united/config
+sudo cp ../src/main/resources/application.properties /etc/med-united/config
+sudo ln -s /etc/med-united/config /opt/med-united/config
+sudo cp med-united.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable med-united.service
 sudo systemctl start med-united
