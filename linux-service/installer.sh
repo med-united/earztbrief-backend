75b334b3d81a775498e453c253173da5f3d365d5
simoneS96 <simone.stifano@incentergy.de>
2022-04-19 12:51:49 +0200

    test deployment

--- linux-service/installer.sh
+++ linux-service/installer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo cp -r ../target/quarkus-app /opt/med-united
 sudo mkdir -p /opt/med-united/application/quarkus-app/
-sudo mkdir -p /etc/med-united/config
-sudo cp ../src/main/resources/application.properties /etc/med-united/config
 sudo ln -s /etc/med-united/config /opt/med-united/config
 sudo cp med-united.service /etc/systemd/system/
 sudo systemctl daemon-reload
--- src/main/java/health/medunited/TestController.java
+++ src/main/java/health/medunited/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GET
     public String performTest() {
-        return "Test7";
+        return "Test8";
     }
     
 }
